878f7a19359b96cbd1485dbacc1138779e43e37c
musale <martinmshale@gmail.com>
2017-02-08 15:11:17 +0300

    :wink: solutions to chapter 7 problems

--- chapter7/main.go
+++ chapter7/main.go
@@ -4,12 +4,34 @@ import "fmt"
 
 func main() {
 	// 1
-	xs := []int{1, 2, 3}
+	xs := []int{
+		48, 96, 86, 68,
+		57, 82, 63, 70,
+		37, 34, 83, 27,
+		19, 97, 9, 17,
+	}
 	fmt.Println("Sum: ", sum(xs...))
 
 	// 2
-	fmt.Println("ODD ", halve(1))
-	fmt.Println("EVEN ", halve(2))
+	fmt.Println(halve(1))
+	fmt.Println(halve(2))
+
+	// 3
+	fmt.Println(greatestAlive(xs...))
+
+	// 4
+	nextOdd := makeOddGenerator()
+	fmt.Println(nextOdd())
+	fmt.Println(nextOdd())
+	fmt.Println(nextOdd())
+
+	// 5
+	fmt.Println(fib(0))
+	fmt.Println(fib(1))
+	fmt.Println(fib(7))
+
+	// 6.
+	fmt.Println(defs())
 }
 
 func sum(args ...int) int {
@@ -26,13 +48,49 @@ func halve(args int) (int, bool) {
 	// Write a function which takes an integer and halves it and returns true if
 	// it was even or false if it was odd. }
 
-	half = args / 2
+	half := args / 2
 
-	if half % 2 == 0 {
+	if half%2 == 0 {
 		// even
-		return half, true
+		return half, false
 	} else {
 		// odd
-		return half, false
+		return half, true
 	}
+}
+
+func greatestAlive(args ...int) int {
+	// Write a function with one variadic parameter that finds the greatest
+	// number in a list of numbers.
+
+	greatest := args[0]
+	for _, value := range args {
+		if value >= greatest {
+			greatest = value
+		}
+	}
+
+	return greatest
+}
+
+func makeOddGenerator() func() uint {
+	//  write a makeOddGenerator function that generates odd numbers.
+	i := uint(1)
+	return func() (ret uint) {
+		ret = i
+		i += 2
+		return
+	}
+}
+func fib(args int) int {
+	//  Write a recursive function which can find fib(n)
+	if args == 0 || args == 1 {
+		return args
+	}
+	return fib(args-1) + fib(args-2)
+}
+
+func defs() string {
+	definitions := "\n a defer is a schedule for a function to be called when the function finishes running.\n a panic is a programmer error that is not easy to recover from.\n a recover is a way used to handle panic errors."
+	return definitions
 }
